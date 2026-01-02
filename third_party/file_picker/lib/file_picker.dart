a2076d3b073451ed7b0216988cfb284ddfc35ef5
Nokitomo <aronnegiuseppe@gmail.com>
2026-01-02 09:07:52 +0100

    Esporta classi file_picker desktop

--- third_party/file_picker/lib/file_picker.dart
+++ third_party/file_picker/lib/file_picker.dart
@@ -3,3 +3,6 @@ library file_picker;
 export './src/file_picker.dart';
 export './src/platform_file.dart';
 export './src/file_picker_result.dart';
+export './src/linux/file_picker_linux.dart';
+export './src/file_picker_macos.dart';
+export './src/windows/file_picker_windows.dart';
